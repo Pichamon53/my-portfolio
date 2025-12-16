13962cadef05da5590d147d062815639b89819c7
pichamon53 <pichada1653@outlook.com>
2025-12-16 15:21:17 +0700

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
-// tailwind.config.js
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{js,ts,jsx,tsx}'], 
+  darkMode: 'class', // ใช้ dark mode ผ่าน class
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],  // กำหนดไฟล์ที่ Tailwind จะคอมไพล์
   theme: {
-  extend: {
-    fontFamily: {
-      sarabun: ['"Sarabun"', 'sans-serif'],
-    },
-    animation: {
-      blob: "blob 7s infinite",
-    },
-    keyframes: {
-      blob: {
-        "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
-        "33%": { transform: "translate(30px, -50px) scale(1.1)" },
-        "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+    extend: {
+      fontFamily: {
+        sarabun: ['"Sarabun"', 'sans-serif'],  // ใช้ฟอนต์ Sarabun
+      },
+      animation: {
+        blob: "blob 7s infinite",  // กำหนด animation blob
+      },
+      keyframes: {
+        blob: {
+          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+        },
       },
     },
   },
-},
   plugins: [],
 };
